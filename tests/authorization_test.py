d14ef9d1d25c58aea96eb8e6f07511766e470c6b
Rustam <kodzokoff2016@yandex.ru>
2024-03-11 16:39:54 +0300

    rename def

--- tests/authorization_test.py
+++ tests/authorization_test.py
@@ -2,7 +2,7 @@ from selene import browser
 from selene.core import command
 
 
-def test_profession_searching():
+def test_start_page_auth():
     browser.open('https://okko.tv/')
     browser.element('[test-id="nav_sign"]').click()
     browser.element('[type="submit"]').click()
